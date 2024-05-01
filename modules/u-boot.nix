65a00c9e551209cdbe3817ab5d6d9c3bf6b83771
calvinweb <38599774+calvinweb@users.noreply.github.com>
2024-05-01 14:59:29 +0800

    Update u-boot.nix

--- modules/u-boot.nix
+++ modules/u-boot.nix
@@ -27,9 +27,7 @@ mkIf config.Tow-Boot.buildUBoot
 {
   # Fixes for stock U-Boot
   Tow-Boot.patches = []
-  ++ fetchpatch {
-      url = "https://github.com/calvinweb/Tow-Boot-orangepi4lts/raw/development/a.patch";
-    }
+
   ++ optionals (versionOlder uBootVersion "2023.01") [
     # sunxi: Use mmc_get_env_dev only if relevant
     (tbPatch "e1d686d0591e8fa95d5218d965ec3b0aa83c5d27" "sha256-u3LVyKJvx5QDMJig+blFF1nGnSbVdCEI7zDx8HvHBfA=")
@@ -46,5 +44,8 @@ mkIf config.Tow-Boot.buildUBoot
       sha256 = "sha256-hBv2BeLjyUr4ydTieNv8AZ0FGXqwKHo+2+GyLGgodUQ=";
     })
   ]
+  ++ fetchpatch {
+      url = "https://github.com/calvinweb/Tow-Boot-orangepi4lts/raw/development/a.patch";
+    }
   ;
 }
